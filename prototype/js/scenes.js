1639a70762e087d19e9dd13c4f2eb22dc1c3053b
Zach <Zach@BENDER>
2012-09-03 15:20:33 -0700

    fix aiming bugs, mouselook bugs

--- prototype/js/scenes.js
+++ prototype/js/scenes.js
@@ -42,15 +42,15 @@ Crafty.scene("main", function () {
     addObstacles();
 	
     //create our player entity with some premade components
-    var player1 = Crafty.e("2D, DOM, Box2D, Character, player_bman, PlayerControl, RangedAttacker, projectileAttacker")
-            .attr({ x: 80, y: 0, z: 5 })
+    var player1 = Crafty.e("2D, DOM, Box2D, Character, player_bman, Mouse, PlayerControl, RangedAttacker, projectileAttacker")
+            .attr({ x: 80, y: 250, z: 5 })
             .PlayerControl(2, 20)
             .RangedAttacker()
 //            .BombDropper()
             .Character(1);
     
-    var player2 = Crafty.e("2D, DOM, Box2D, Character, player_iman, PlayerControl, lineOfSightAttacker")
-	        .attr({ x: 800, y: 0, z: 5 })
+    var player2 = Crafty.e("2D, DOM, Box2D, Character, player_iman, Mouse, PlayerControl, lineOfSightAttacker")
+	        .attr({ x: 800, y: 70, z: 5 })
             .PlayerControl(2, 4)
 	        .Character(2);
     		
@@ -72,116 +72,8 @@ Crafty.scene("main", function () {
 
     Crafty.bind("Firing", function(player) {
     	// bind new mouse events
-        $(this).mousemove(function(event) {
-        	Crafty.trigger("CheckFiring", [event, player]);
-        });
-        $(this).mouseup(function(event) {
-        	Crafty.trigger("FireProjectile", [event, player]);
-        });
-    });
-
-    Crafty.bind("FireProjectile", function(args) {
-    	var event = args[0];
-    	var player = args[1];
-		var translatedPosition = Crafty.DOM.translate(event.clientX, event.clientY);
-    	
-    	if(player.isFiring && player.powerAmount > 0) {
-            var translatedAngle = Math.atan2(translatedPosition.y - player.y, translatedPosition.x - player.x);
-    		var rotationAngle = translatedAngle - Math.PI;	// rotation angle needs to be mapped to take into account the DOM origin
-    		console.log("firing at " + (rotationAngle * 180 / Math.PI));
-    		
-    		player.isFiring = false;
-    		if(player.has("projectileAttacker")) {
-                var projectile = Crafty.e("2D, DOM, Box2D, ExplodingProjectile")
-    						            .attr({ x: player.x + Crafty.viewport.x + player.faceDirection * 16, y: player.y, z: 2 })
-    						            .ExplodingProjectile("projectile", player);
-
-                projectile.Throw(50 * player.powerAmount, rotationAngle);
-    		}
-    		else if(player.has("lineOfSightAttacker")) {
-                var los = Crafty.e("2D, DOM, Box2D, LineOfSightAttack")
-							            .attr({ x: player.x + Crafty.viewport.x + player.faceDirection * 16, y: player.y - 16, z: 2 })
-    						            .LineOfSightAttack("lineofsight", player);
-
-                los.Attack(50 * player.powerAmount, rotationAngle);
-    		}
-    		
-    		player.powerAmount = 0;
-    		Crafty.trigger("UpdatePower", player);
-    		
-    		// unbind mouse events and re-enable mouselook
-            $(this).unbind('mouseup');
-            $(this).unbind('mousemove');
-        	Crafty.viewport.mouselook(true);
-        	
-        	// remove slingshot
-    		Crafty("Slingshot").destroy();
-    		Crafty("Canvas").destroy();
-    		
-    		// end current turn
-            endTurn();
-    	}
-    });
-    
-    Crafty.bind("CheckFiring", function(args) {
-    	var event = args[0];
-    	var player = args[1];
-		var translatedPosition = Crafty.DOM.translate(event.clientX, event.clientY);
-    	
-    	if(player.isFiring) {
-    		// calculate distance between mouse cursor and player
-    		// x^2 + y^2 = z^2
-    		// the pixel distance is translated to "power" in increments of 20
-    		var distance = Math.sqrt(Math.pow(translatedPosition.x - player.x, 2) + Math.pow(translatedPosition.y - player.y, 2));
-    		if(distance > 100) {
-    			player.powerAmount = 5;
-    		}
-    		else {
-    			player.powerAmount = Math.floor(distance / 20);
-    		}
-    		
-    		// calculate color intensity based on power
-    		var intensity;
-    		switch(player.powerAmount) {
-        		case 1:
-        			intensity = "#0055bb";
-        			break;
-        		case 2:
-        			intensity = "#00dd00";
-        			break;
-        		case 3:
-        			intensity = "#ffff33";
-        			break;
-        		case 4:
-        			intensity = "#ff8811";
-        			break;
-        		default:
-        			intensity = "#bb0033";
-    		}
-
-
-    		// remove any existing slingshots and draw a new one
-            var translatedAngle = Math.atan2(translatedPosition.y - player.y, translatedPosition.x - player.x);
-    		var rotationAngle = translatedAngle - Math.PI;	// rotation angle needs to be mapped to take into account the DOM origin
-    		Crafty("Slingshot").destroy();
-    		Crafty.e("Slingshot")
-	    		.Slingshot(player.x, player.y, distance, intensity)
-	    		.attach(Crafty.e("SlingshotAnchor").SlingshotAnchor(translatedPosition.x, translatedPosition.y, intensity))
-	    		.attach(Crafty.e("SlingshotArrow").SlingshotArrow(2 * (player.x) - translatedPosition.x - Crafty.viewport.x, 2 * (player.y) - translatedPosition.y - Crafty.viewport.y, rotationAngle, intensity));
-        	
-    		Crafty.trigger("UpdatePower", player);
-
-    		// if the mouse is behind the character, swivel
-    		if(translatedPosition.x > player.x) {
-    			player.trigger("FaceNewDirection", -1);
-    		}
-    		else {
-    			player.trigger("FaceNewDirection", 1);
-    		}
-    		
-//    		console.log("angle: " + (translatedAngle * 180 / Math.PI));
-//    		console.log("actual angle: " + (rotationAngle * 180 / Math.PI));
-    	}
+        $(document).on('mousemove', {player: player}, checkFiring);
+        $(document).on('mouseup', {player: player}, fireProjectile);
     });
 
     Crafty.bind("GameOver", function(loser) {
@@ -192,4 +84,118 @@ Crafty.scene("main", function () {
     });
 
 	Crafty.trigger("UpdateTurn");
+	
+    $(document).mousemove(function(event) {
+		var translatedPosition = Crafty.DOM.translate(event.clientX, event.clientY);
+    	$('#mousex .text').text(translatedPosition.x);
+    	$('#mousey .text').text(translatedPosition.y);
+    });
+    
+    // sometimes Crafty initializes with incorrect stage attributes, shifting the actual level
+    // if this is the case when the scene first loads, reload the viewport
+    if(Crafty.stage.x != 0 && Crafty.stage.y != 0) {
+    	Crafty.viewport.reload();
+    }
 });
+
+var checkFiring = function(event, player) {
+	var player = event.data.player;
+	var translatedPosition = Crafty.DOM.translate(event.clientX, event.clientY);
+	
+	if(player.isFiring) {
+		// calculate distance between mouse cursor and player
+		// x^2 + y^2 = z^2
+		// the pixel distance is translated to "power" in increments of 20
+		var distance = Math.sqrt(Math.pow(translatedPosition.x - player.x, 2) + Math.pow(translatedPosition.y - player.y, 2));
+		if(distance > 100) {
+			player.powerAmount = 5;
+		}
+		else {
+			player.powerAmount = Math.floor(distance / 20);
+		}
+		
+		// calculate color intensity based on power
+		var intensity;
+		switch(player.powerAmount) {
+    		case 1:
+    			intensity = "#0055bb";
+    			break;
+    		case 2:
+    			intensity = "#00dd00";
+    			break;
+    		case 3:
+    			intensity = "#ffff33";
+    			break;
+    		case 4:
+    			intensity = "#ff8811";
+    			break;
+    		default:
+    			intensity = "#bb0033";
+		}
+
+
+		// remove any existing slingshots and draw a new one
+        var translatedAngle = Math.atan2(translatedPosition.y - player.y, translatedPosition.x - player.x);
+		var rotationAngle = translatedAngle - Math.PI;	// rotation angle needs to be mapped to take into account the DOM origin
+		Crafty("Slingshot").destroy();
+		Crafty.e("Slingshot")
+    		.Slingshot(player.x, player.y, distance, intensity)
+    		.attach(Crafty.e("SlingshotAnchor").SlingshotAnchor(translatedPosition.x, translatedPosition.y, intensity))
+    		.attach(Crafty.e("SlingshotArrow").SlingshotArrow(2 * (player.x) - translatedPosition.x - Crafty.viewport.x, 2 * (player.y) - translatedPosition.y - Crafty.viewport.y, rotationAngle, intensity));
+    	
+		Crafty.trigger("UpdatePower", player);
+
+		// if the mouse is behind the character, swivel
+		if(translatedPosition.x > player.x) {
+			player.trigger("FaceNewDirection", -1);
+		}
+		else {
+			player.trigger("FaceNewDirection", 1);
+		}
+		
+//		console.log("angle: " + (translatedAngle * 180 / Math.PI));
+//		console.log("actual angle: " + (rotationAngle * 180 / Math.PI));
+	}
+};
+
+var fireProjectile = function(event) {
+	var player = event.data.player;
+	var translatedPosition = Crafty.DOM.translate(event.clientX, event.clientY);
+	
+	if(player.isFiring && player.powerAmount > 0) {
+        var translatedAngle = Math.atan2(translatedPosition.y - player.y, translatedPosition.x - player.x);
+		var rotationAngle = translatedAngle - Math.PI;	// rotation angle needs to be mapped to take into account the DOM origin
+		console.log("firing at " + (rotationAngle * 180 / Math.PI));
+		
+		player.isFiring = false;
+		if(player.has("projectileAttacker")) {
+            var projectile = Crafty.e("2D, DOM, Box2D, ExplodingProjectile")
+						            .attr({ x: player.x + player.faceDirection * 16, y: player.y, z: 2 })
+						            .ExplodingProjectile("projectile", player);
+
+            projectile.Throw(50 * player.powerAmount, rotationAngle);
+		}
+		else if(player.has("lineOfSightAttacker")) {
+            var los = Crafty.e("2D, DOM, Box2D, LineOfSightAttack")
+						            .attr({ x: player.x + player.faceDirection * 16, y: player.y - 16, z: 2 })
+						            .LineOfSightAttack("lineofsight", player);
+
+            los.Attack(50 * player.powerAmount, rotationAngle);
+		}
+		
+		player.powerAmount = 0;
+		Crafty.trigger("UpdatePower", player);
+		
+		// unbind mouse events and re-enable mouselook
+        $(document).off('mousemove', checkFiring);
+        $(document).off('mouseup', fireProjectile);
+    	Crafty.viewport.mouselook(true);
+    	
+    	// remove slingshot
+		Crafty("Slingshot").destroy();
+		Crafty("Canvas").destroy();
+		
+		// end current turn
+        endTurn();
+	}
+};
